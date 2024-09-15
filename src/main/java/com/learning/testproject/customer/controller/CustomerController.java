1cce6b0c38b957ba64e25754b5fa833ab72d0b58
Arslane Guettaf <irontitou5@gmail.com>
2024-09-15 10:53:37 -0400

    Update CustomerController.java

--- src/main/java/com/learning/testproject/customer/controller/CustomerController.java
+++ src/main/java/com/learning/testproject/customer/controller/CustomerController.java
@@ -7,8 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-// test
-
 @RestController
 @RequestMapping("/api/v1/customers")
 @RequiredArgsConstructor
